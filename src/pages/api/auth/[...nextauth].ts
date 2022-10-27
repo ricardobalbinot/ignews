f489acdfb520fb67885d582b0908844073f49757
Ricardo Balbinot <ricardo.balbinot@hotmail.com>
2022-10-27 05:23:22 -0300

    create stripe checkout session

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -1,5 +1,9 @@
+import { query as q } from 'faunadb'
+
 import NextAuth from 'next-auth'
 import GithubProvider from 'next-auth/providers/github'
+
+import { fauna } from '../../../services/fauna'
 
 export const authOptions = {
   providers: [
@@ -8,6 +12,41 @@ export const authOptions = {
       clientSecret: process.env.GITHUB_CLIENT_SECRET,
     }),
   ],
+  secret: process.env.NEXT_AUTH_SECRET,
+  callbacks: {
+    async signIn({ user }) {
+      const { email } = user
+
+      try {
+        await fauna.query(
+          q.If(
+            q.Not(
+              q.Exists(
+                q.Match(
+                  q.Index('user_by_email'),
+                  q.Casefold(email)
+                )
+              )
+            ),
+            q.Create(
+              q.Collection('users'),
+              { data: { email } } 
+            ),
+            q.Get(
+              q.Match(
+                q.Index('user_by_email'),
+                q.Casefold(email)
+              )
+            )
+          )
+        )
+  
+        return true
+      } catch {
+        return false
+      }
+    },
+  }
 }
 
 export default NextAuth(authOptions)
